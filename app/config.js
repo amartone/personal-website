4c63f489ac243d659142bdd64f4c31df5da93098
Andrew <amartone92@gmail.com>
2016-06-08 03:04:13 -0400

    Initial Commit

---
+++ app/config.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Andrew on 2/24/16.
+ */
+
+'use strict';
+
+//Set up URL routes for each controller.
+app.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'views/home/home.view.html',
+			controller: 'HomeController',
+			controllerAs: 'model'
+		});
+});
