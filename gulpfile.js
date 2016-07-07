afb8604f78f24f40b78fd6316e49a50a68fe48c4
Andrew <amartone92@gmail.com>
2016-07-07 01:09:48 -0400

    Modifying education section, footer css, and gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -89,9 +89,9 @@ gulp.task('bootstrap-scripts', function() {
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
-		return gulp.src(['app/scripts/particles.js','app/scripts/app.js'])
+		return gulp.src(['app/scripts/particles.js','app/scripts/app.js', 'app/scripts/matrix-text.js'])
 				// .pipe(concat('all.min.js'))
-				// .pipe(uglify())
+				.pipe(uglify())
 				.pipe(gulp.dest('dist/scripts/'));
 });
 
