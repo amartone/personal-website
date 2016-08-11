20bbec4492a4419deb77a48a5bb9f1251e250b54
Andrew <amartone92@gmail.com>
2016-08-11 02:46:30 -0400

    Fixing nabber highlight

--- config.js
+++ config.js
@@ -5,9 +5,12 @@
 'use strict';
 
 //Set up URL routes for each controller.
-app.config(function($routeProvider) {
+app.config(function($routeProvider, $locationProvider) {
 	$routeProvider
 		.when('/', {
 			templateUrl: 'views/home/home.view.html'
 		});
+
+	$locationProvider.html5Mode(true);
+
 });
